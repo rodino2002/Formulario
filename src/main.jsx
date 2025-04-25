302e4f73d68ac6193d18c0b380ed7a9652c05961
felicianorodino <rodinofeliciano2002@gmail.com>
2025-04-25 14:39:30 +0100

    eliminando as variaveis inuteis

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import './index.css'
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { Routers } from './routes'
 import App from './pages/App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
